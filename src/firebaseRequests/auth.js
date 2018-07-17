fe04b4feaaf64e6f94692eb1f3b95ffeea4ea66f
Maken Hubbard <makhub26@outlook.com>
2018-07-16 19:36:17 -0500

    adding the key for firebase

--- src/firebaseRequests/auth.js
+++ src/firebaseRequests/auth.js
@@ -1,5 +1,7 @@
-// import firebase from 'firebase';
+import firebase from 'firebase';
 
-// const registerUser = (user) => {
-//   return firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
-// };
+const registerUser = (user) => {
+  return firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
+};
+
+export default {registerUser};
