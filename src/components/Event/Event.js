3f60ea5dc88f8a4808a86359c2580ca8479e2876
Maken Hubbard <makhub26@outlook.com>
2018-08-10 09:24:29 -0500

    adding style change to the events page

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -44,26 +44,26 @@ class Event extends React.Component {
       <div className="jumbotron">
         <div className="media">
           <div className="media-left">
-            <img className="media-object" src={event.imageUrl} alt={event.imageUrl}/>
+            <img className="media-object" src={event.imageUrl} alt={event.imageUrl} />
           </div>
           <div className="media-body">
-            <h4 className="media-heading">{event.eventAttending}</h4>
+            <h4 className="event-heading">{event.eventAttending}</h4>
             <ul id="eventsList">
-              <li className="eventtext-center" >
-                <span className="col-xs-4">{event.address} <br /> {event.city}, {event.state} {event.zip}</span>
-                <span className="col-xs-3">From: {event.arrival}</span>
-                <span className="col-xs-3">To: {event.departure}</span>
+              <li className="eventText-center" >
+                <span  id="addressInfo" className="col-xs-10">{event.address} <br /> {event.city}, {event.state} {event.zip}</span>
+                <span id="toTime" className="col-xs-4 col-xs-offset-1">From: {event.arrival}</span>
+                <span id="fromTime" className="col-xs-4 col-xs-offset-1">To: {event.departure}</span>
               </li>
             </ul>
           </div>
-        </div>
-        <div className="row eventsPageButtons" >
-          <button className="editEvent col-xs-2" onClick={this.routeToEditPage}>
-            <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-          </button>
-          <button className="deleteEvent  col-xs-2" onClick={this.deleteEvent}>
-            <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
-          </button>
+          <div className="row eventsPageButtons" >
+            <button className="editEvent col-xs-2 col-xs-offset-5" onClick={this.routeToEditPage}>
+              <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+            </button>
+            <button className="deleteEvent  col-xs-2 col-xs-offset-2" onClick={this.deleteEvent}>
+              <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+            </button>
+          </div>
         </div>
       </div>
     );
