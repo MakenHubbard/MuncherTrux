15e86b0db71a3f881e1cf760e325bec8ed06185c
Maken Hubbard <makhub26@outlook.com>
2018-08-17 17:25:38 -0500

    adding styles to the new truck page

--- src/components/NewTruck/NewTruck.js
+++ src/components/NewTruck/NewTruck.js
@@ -49,39 +49,41 @@ class NewTruck extends React.Component {
     this.infoFormField('imageUrl', e);
   };
 
-  render () {
+  render  () {
     const { newTruck } = this.state;
     return (
-      <div className="NewTruck">
-        <div className="container">
-          <form>
-            <div>
-              <div className="form-group">
-                <label htmlFor="nameOfCompany">Company Name: </label>
-                <input type="text" className="form-control" id="name" placeholder="" value={newTruck.name} onChange={this.nameChange} />
+      <div>
+        <h1 id="newTruckTitle">We need some Information from you</h1>
+        <div className="NewTruck" id="regTruckFormBack">
+          <div className="container" id="regTruckForm">
+            <form>
+              <div>
+                <div className="form-group">
+                  <label htmlFor="nameOfCompany">Company Name: </label>
+                  <input type="text" className="form-control" id="name" placeholder="" value={newTruck.name} onChange={this.nameChange} />
+                </div>
               </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="bioField">About: </label>
-              <input type="text" className="form-control" id="bio" placeholder="What would you like us to know about your company?" value={newTruck.bio} onChange={this.bioChange} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="imgUrl">Image Url: </label>
-              <input type="text" className="form-control" id="imageUrl" placeholder="" value={newTruck.imageUrl} onChange={this.imageUrlChange} />
-            </div>
-            <div className="col-sm-6">
-              <button
-                type="submit"
-                className="btn btn-default col-xs-12"
-                onClick={this.saveNewTruckEvent}
-              >
-                Save Truck
-              </button>
-            </div>
-          </form>
+              <div className="form-group">
+                <label htmlFor="bioField">About: </label>
+                <input type="text" className="form-control" id="bio" placeholder="What would you like us to know about your company?" value={newTruck.bio} onChange={this.bioChange} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="imgUrl">Image Url: </label>
+                <input type="text" className="form-control" id="imageUrl" placeholder="" value={newTruck.imageUrl} onChange={this.imageUrlChange} />
+              </div>
+              <div className="col-sm-6">
+                <button
+                  type="submit"
+                  className="btn btn-default col-xs-12 saveRegTruckBtn"
+                  onClick={this.saveNewTruckEvent}
+                >
+                  Save Truck
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-
     );
   }
 }
